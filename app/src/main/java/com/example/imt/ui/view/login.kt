ff29454cdc002b78d90f9bc774a5428f6e3a4d40
joevandy <kochenkbiru@gmail.com>
2025-10-22 09:30:43 +0700

    coba edit -jae

--- app/src/main/java/com/example/imt/ui/view/login.kt
+++ app/src/main/java/com/example/imt/ui/view/login.kt
@@ -1,17 +1,19 @@
 package com.example.imt.ui.view
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun login(){
-    Column (){
-        Text(
-            text = "Pangeran Jember"
-        )
-    }
+    Text(
+        text = "halo semuanya aku jae"
+    )
+
+    Text(
+        text = "coba edit ini sapa aja"
+    )
 }
 
 @Composable
